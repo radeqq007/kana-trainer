2998ecfe791250f6d86059167ce108286fdf3f9b
radeqq007 <radeqq007@interia.pl>
2025-12-18 19:55:56 +0100

    feat: setup router

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,9 +1,19 @@
 import "@renderer/assets/main.css";
+import { Route, Routes } from "react-router-dom";
 
 function App(): React.JSX.Element {
   return (
-    <div className="bg-background text-foreground">
-      <h1>Hello, world!</h1>
+    <div className="bg-background text-foreground min-h-screen">
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="p-4">
+              <h1>Hello, world!</h1>
+            </div>
+          }
+        />
+      </Routes>
     </div>
   );
 }
