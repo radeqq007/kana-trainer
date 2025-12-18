90ebaf0cda7b9b9cd73bd7a1d7849d8bac19cdd0
radeqq007 <radeqq007@interia.pl>
2025-12-18 19:59:04 +0100

    feat: setup home page

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,18 +1,12 @@
 import "@renderer/assets/main.css";
+import Home from "@renderer/pages/Home";
 import { Route, Routes } from "react-router-dom";
 
 function App(): React.JSX.Element {
   return (
     <div className="bg-background text-foreground min-h-screen">
       <Routes>
-        <Route
-          path="/"
-          element={
-            <div className="p-4">
-              <h1>Hello, world!</h1>
-            </div>
-          }
-        />
+        <Route path="/" element={<Home />} />
       </Routes>
     </div>
   );
