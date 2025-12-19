757e4564533e3036e5e2a8168bab2686bd2681ee
radeqq007 <radeqq007@interia.pl>
2025-12-19 13:35:55 +0100

    feat: setup home page

--- src/renderer/src/pages/Home.tsx
+++ src/renderer/src/pages/Home.tsx
@@ -1,7 +1,19 @@
+import { Button } from "@renderer/components/ui/button";
+import { Link } from "react-router-dom";
+
 const Home = (): React.JSX.Element => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="flex h-screen items-center justify-center flex-col gap-4">
+      <h1 className="text-9xl font-bold">あ</h1>
+      <h2 className="text-7xl font-medium">Kana Trainer</h2>
+      <span className="flex gap-2 mt-10">
+        <Button variant="secondary">
+          <Link to="/characters">Change Characters</Link>
+        </Button>
+        <Button variant="default">
+          <Link to="/play">Start</Link>
+        </Button>
+      </span>
     </div>
   );
 };
