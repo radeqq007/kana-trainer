0bad989d303e5672b959e51bfd6ec4e03f4729d4
radeqq007 <radeqq007@interia.pl>
2025-12-20 00:06:21 +0100

    fix: fix electron-store

--- src/main/index.ts
+++ src/main/index.ts
@@ -29,8 +29,6 @@ function createWindow(): void {
     return { action: "deny" };
   });
 
-  // HMR for renderer base on electron-vite cli.
-  // Load the remote URL for development or the local html file for production.
   if (is.dev && process.env["ELECTRON_RENDERER_URL"]) {
     mainWindow.loadURL(process.env["ELECTRON_RENDERER_URL"]);
   } else {
@@ -38,59 +36,59 @@ function createWindow(): void {
   }
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  // Set app user model id for windows
-  electronApp.setAppUserModelId("com.electron");
-
-  // Default open or close DevTools by F12 in development
-  // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
-  app.on("browser-window-created", (_, window) => {
-    optimizer.watchWindowShortcuts(window);
-  });
-
-  // IPC test
-  ipcMain.on("ping", () => console.log("pong"));
-
-  createWindow();
-
-  app.on("activate", function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
-});
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+// I know this sucks but otherwise it doesn't work
+let store: any;
+async function initStore(): Promise<any> {
+  const { default: Store } = await import("electron-store");
+  store = new Store();
+}
 
-const store = new Store({
-  defaults: {
-    hiragana: Object.keys(chars.hiragana),
-    katakana: Object.keys(chars.katakana),
-  },
-});
+function registerIpcHandlers(): void {
+  ipcMain.handle("store:get", (_event, key) => {
+    return store.get(key);
+  });
 
-ipcMain.handle("store:get", (_event, key) => {
-  return store.get(key);
-});
+  ipcMain.handle("store:set", (_event, key, value) => {
+    store.set(key, value);
+  });
 
-ipcMain.handle("store:set", (_event, key, value) => {
-  store.set(key, value);
-});
+  ipcMain.handle("store:delete", (_event, key) => {
+    store.delete(key);
+  });
+}
 
-ipcMain.handle("store:delete", (_event, key) => {
-  store.delete(key);
+app.whenReady().then(() => {
+  electronApp.setAppUserModelId("com.electron");
+
+  app.on("browser-window-created", (_, window) => {
+    optimizer.watchWindowShortcuts(window);
+  });
+
+  initStore();
+
+  registerIpcHandlers();
+
+  // default values
+  try {
+    if (!store.has("hiragana") && chars?.hiragana) {
+      store.set("hiragana", Object.keys(chars.hiragana));
+    }
+    if (!store.has("katakana") && chars?.katakana) {
+      store.set("katakana", Object.keys(chars.katakana));
+    }
+  } catch (err) {
+    console.error("Failed to initialize store defaults:", err);
+  }
+
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
 });
