961939aa3d6d078df0c28dc9137a71937f242caa
radeqq007 <radoslaw.kaczmarczyk@proton.me>
2025-12-25 18:30:50 +0100

    refactor: simplify preload script

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -2,23 +2,25 @@ import { electronAPI } from "@electron-toolkit/preload";
 import { contextBridge, ipcRenderer } from "electron";
 
 const api = {};
+const storeApi = {
+  get: (key: string) => ipcRenderer.invoke("store:get", key),
+  set: (key: string, value: unknown) => ipcRenderer.invoke("store:set", key, value),
+  delete: (key: string) => ipcRenderer.invoke("store:delete", key),
+};
 
 if (process.contextIsolated) {
   try {
     contextBridge.exposeInMainWorld("electron", electronAPI);
     contextBridge.exposeInMainWorld("api", api);
+    contextBridge.exposeInMainWorld("store", storeApi);
   } catch (error) {
     console.error(error);
   }
 } else {
-  // @ts-ignore (define in dts)
+  // @ts-ignore (defined in dts)
   window.electron = electronAPI;
-  // @ts-ignore (define in dts)
+  // @ts-ignore (defined in dts)
   window.api = api;
+  // @ts-ignore (defined in dts)
+  window.store = storeApi;
 }
-
-contextBridge.exposeInMainWorld("store", {
-  get: (key: string) => ipcRenderer.invoke("store:get", key),
-  set: (key: string, value: unknown) => ipcRenderer.invoke("store:set", key, value),
-  delete: (key: string) => ipcRenderer.invoke("store:delete", key),
-});
